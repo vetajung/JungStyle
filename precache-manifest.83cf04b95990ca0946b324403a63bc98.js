8b4ccd4c222b1291e4e09906131a49923a2f9b98
vetajung <pkiyy@naver.com>
2020-05-30 14:29:41 +0900

    Updates

---
+++ precache-manifest.83cf04b95990ca0946b324403a63bc98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d7573b7e9238fc2db1e605c0b74c1b",
+    "url": "/copy_style/index.html"
+  },
+  {
+    "revision": "732b27e03312d266653c",
+    "url": "/copy_style/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ce693b53d9ef8a394c44",
+    "url": "/copy_style/static/js/2.7757fe6b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/copy_style/static/js/2.7757fe6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "732b27e03312d266653c",
+    "url": "/copy_style/static/js/main.9151f19d.chunk.js"
+  },
+  {
+    "revision": "063a37a6a9a4f406054b",
+    "url": "/copy_style/static/js/runtime-main.2d98e9da.js"
+  }
+]);
